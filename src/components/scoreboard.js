e7277646e075e6819bf848418133aa8ef434d458
Patrick <ctrlaltpat@gmail.com>
2018-11-14 10:41:06 +0000

    using points instead of seconds

--- src/components/scoreboard.js
+++ src/components/scoreboard.js
@@ -11,12 +11,12 @@ class ScoreBoard {
       scoreList.id = "score-list"
       scoreList.innerHTML = `
         ${this.scores.map(score =>
-         '<li class="score-li">' + score.user_id + ': ' + score.seconds + '</li>'
-        )}
+         '<li class="score-li">' + score.user_id + ': ' + score.points + '</li>'
+        ).join("")}
       `
       this.main.innerHTML = ""
       this.main.appendChild(scoreList)
-    })  
+    }).then(() => this.addListeners())
   }
 
   static addListeners() {
